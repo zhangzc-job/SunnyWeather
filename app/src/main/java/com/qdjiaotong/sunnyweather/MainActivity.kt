91d093702487af18ccf172a1a06fcbe2cf7ecade
zhangzhongchao <winter1999@163.com>
2021-02-03 21:30:10 +0800

    第一次提交

---
+++ app/src/main/java/com/qdjiaotong/sunnyweather/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.qdjiaotong.sunnyweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//https://github.com/zhangzc-job/SunnyWeather.git
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
